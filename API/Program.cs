ff37799f5ccd9643001d0de4f9d8b01f845650bd
adolfojara10 <gavilanesadolfo@gmail.com>
2024-02-07 17:06:52 -0500

    CORS in c#, first request from angular

--- API/Program.cs
+++ API/Program.cs
@@ -12,11 +12,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 //add controllers
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+//this line is to allow cors. it means the browser acn show what it gets from the backend
+//this happens due to the fact that the backend and frontend are hearing in different ports
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.MapControllers();
