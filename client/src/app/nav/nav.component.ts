4def384b640e42e7751e562c7fadc01060afae85
adolfojara10 <gavilanesadolfo@gmail.com>
2024-02-22 17:07:05 -0500

    routing module finished

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,29 +14,34 @@ export class NavComponent implements OnInit {
 
   model: any = {};
   //loggedIn = false;
-  
 
-  constructor(public accountService: AccountService) {
+
+  constructor(public accountService: AccountService, private router: Router, 
+    private toaster: ToastrService) {
 
   }
 
   ngOnInit(): void {
-    
+
   }
 
   login() {
     console.log(this.model);
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
+        //console.log(response);
+        this.router.navigateByUrl("/members");
         //this.loggedIn = true;
       },
-      error: error => console.log(error)
+      //error: error => console.log(error)
+      error: error => this.toaster.error(error.error)
     });
+
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
     //this.loggedIn = false;
   }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -15,7 +16,7 @@ export class RegisterComponent {
 
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toaster: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -26,7 +27,11 @@ export class RegisterComponent {
       next: () => {
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toaster.error(error.error);
+        console.log(error);
+
+      }
     })
   }
 
